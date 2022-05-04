c63213c8f793676d110c98b578d35513074aa5b9
jaujau088 <jaujau088@gmail.com>
2022-05-04 20:12:06 +0200

    Update the testing logic of localStorage data checking

--- src/modules/class-task-list.test.js
+++ src/modules/class-task-list.test.js
@@ -1,27 +1,46 @@
-import TaskList from './class-task-list.js';
+import TaskList, { storageName } from './class-task-list.js';
 
 const taskList = new TaskList();
 const task1 = 'This is task 1';
 const task2 = 'This is task 2';
 
 beforeAll(() => {
-  taskList.addTask(task1);
+
 });
 
-describe('Check add-remove values:', () => {
-  test('task object is properly created', () => {
-    expect(taskList.data).toEqual([{ description: task1, id: 1, completed: false }]);
-  });
-});
+describe('Check whether add and remove methods update the local storage properly:', () => {
+  describe(`Add task: "${task1}"`, () => {
+    test('localStorage is updated', () => {
+      taskList.addTask(task1);
 
-describe('Check whether local storage is being called on:', () => {
-  test('add a task', () => {
-    expect(localStorage.setItem).toHaveBeenCalled();
+      expect(localStorage.setItem).toHaveBeenCalled();
+    });
+
+    test('localStorage.getItem returns the correct value', () => {
+      expect(JSON.parse(localStorage.getItem(storageName)))
+        .toEqual([{ description: task1, id: 1, completed: false }]);
+    });
   });
 
-  test('remove a task', () => {
-    taskList.removeTask(3);
+  describe(`Add task: "${task2}"`, () => {
+    test('localStorage is updated', () => {
+      taskList.addTask(task2);
 
-    expect(localStorage.setItem).toHaveBeenCalled();
+      expect(localStorage.setItem).toHaveBeenCalled();
+    });
+
+    test('localStorage.getItem returns the correct value', () => {
+      expect(JSON.parse(localStorage.getItem(storageName)))
+        .toEqual([
+          { description: task1, id: 1, completed: false },
+          { description: task2, id: 2, completed: false },
+        ]);
+    });
   });
+
+  // test('remove a task', () => {
+  //   taskList.removeTask(3);
+
+  //   expect(localStorage.setItem).toHaveBeenCalled();
+  // });
 });
