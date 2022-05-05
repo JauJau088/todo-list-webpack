820c24153acb519180a99b017289bed2f00b5907
jaujau088 <jaujau088@gmail.com>
2022-05-05 11:34:36 +0200

    Add testing for local storage add items

---
+++ test/add-remove.test.js
@@ -0,0 +1,46 @@
+import TaskList, { storageName } from '../src/modules/class-task-list.js';
+
+const taskList = new TaskList();
+const task1 = 'This is task 1';
+const task2 = 'This is task 2';
+const task3 = 'This is task 3';
+
+// Local storage testing
+describe('Check if localStorage is being used properly:', () => {
+  describe('Add items:', () => {
+    test(`add task: "${task1}"`, () => {
+      taskList.addTask(task1);
+
+      // Check the value in local storage
+      expect(JSON.parse(localStorage.getItem(storageName)))
+        .toEqual([{ description: task1, id: 1, completed: false }]);
+    });
+
+    test(`add task: "${task2}"`, () => {
+      taskList.addTask(task2);
+
+      // Check the value in local storage
+      expect(JSON.parse(localStorage.getItem(storageName)))
+        .toEqual([
+          { description: task1, id: 1, completed: false },
+          { description: task2, id: 2, completed: false },
+        ]);
+    });
+
+    test(`add task: "${task3}"`, () => {
+      taskList.addTask(task3);
+
+      // Check the value in local storage
+      expect(JSON.parse(localStorage.getItem(storageName)))
+        .toEqual([
+          { description: task1, id: 1, completed: false },
+          { description: task2, id: 2, completed: false },
+          { description: task3, id: 3, completed: false },
+        ]);
+    });
+  });
+
+  describe('Remove items:', () => {
+
+  });
+});
